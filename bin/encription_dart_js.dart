45820f78f66e432528a6c61cb97b554b5e925197
shawon1fb <shawon0187@gmail.com>
2022-03-28 02:11:42 +0600

    init 2

--- bin/encription_dart_js.dart
+++ bin/encription_dart_js.dart
@@ -2,7 +2,7 @@ import 'package:encription_dart_js/encription_dart_js.dart' as en;
 
 void main(List<String> arguments) {
   String d = en.decryptAESCryptoJS(
-      encrypted: 'U2FsdGVkX1/5/QyqowjjI7UJksazXJTEEuk/0u5Pnck=',
+      encrypted: 'U2FsdGVkX1/5yryGaKLlSZ2q/7nfMu3BQKSv5V62I4s=',
       passphrase: 'secret key 123');
   print(d);
 }
--- type_script/index.ts
+++ type_script/index.ts
@@ -1,8 +1,8 @@
 import * as CryptoJS from "crypto-js";
 
 // Encrypt
-const ciphertext = CryptoJS.AES.encrypt('my message', 'secret key 123').toString();
-
+const ciphertext = CryptoJS.AES.encrypt('my message2', 'secret key 123').toString();
+console.log(ciphertext);
 // Decrypt
 const bytes = CryptoJS.AES.decrypt(ciphertext, 'secret key 123');
 const originalText = bytes.toString(CryptoJS.enc.Utf8);
